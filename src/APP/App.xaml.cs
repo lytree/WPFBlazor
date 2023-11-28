b6e3b1deb50a099030f4596f0131b37abf17860e
lytree <hi.yangzh@outlook.com>
2023-11-28 14:33:08 +0800

    add HandyControls

--- src/APP/App.xaml.cs
+++ src/APP/App.xaml.cs
@@ -1,17 +1,84 @@
-﻿using System;
+﻿using APP.Helper;
+using HandyControl.Tools;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace APP
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class AppStart : Application
-    {
-    }
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class AppStart : Application
+	{
+#pragma warning disable IDE0052
+		[SuppressMessage("ReSharper", "NotAccessedField.Local")]
+		private static Mutex AppMutex;
+#pragma warning restore IDE0052
+		public AppStart()
+		{
+
+			var cachePath = $"{AppDomain.CurrentDomain.BaseDirectory}Cache";
+			if (!Directory.Exists(cachePath))
+			{
+				Directory.CreateDirectory(cachePath);
+			}
+			ProfileOptimization.SetProfileRoot(cachePath);
+			ProfileOptimization.StartProfile("Profile");
+
+
+		}
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			AppMutex = new Mutex(true, "APP", out var createdNew);
+			if (!createdNew)
+			{
+				var current = Process.GetCurrentProcess();
+
+				foreach (var process in Process.GetProcessesByName(current.ProcessName))
+				{
+					if (process.Id != current.Id)
+					{
+						Win32Helper.SetForegroundWindow(process.MainWindowHandle);
+						break;
+					}
+				}
+				Shutdown();
+			}
+			else
+			{
+				var splashScreen = new SplashScreen("Resources/Img/Cover.png");
+				splashScreen.Show(true);
+
+				base.OnStartup(e);
+
+				ApplicationHelper.IsSingleInstance();
+
+				ShutdownMode = ShutdownMode.OnMainWindowClose;
+
+				ConfigHelper.Instance.SetWindowDefaultStyle();
+				ConfigHelper.Instance.SetNavigationWindowDefaultStyle();
+
+
+				ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+			}
+
+			base.OnStartup(e);
+		}
+		protected override void OnExit(ExitEventArgs e)
+		{
+
+			base.OnExit(e);
+		}
+	}
 }
--- src/APP/MainWindow.xaml.cs
+++ src/APP/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using APP.Extensions;
 using APP.Infrastructure.Constant;
 using APP.Infrastructure.Events;
+using APP.UserControl;
 using Autofac;
 using Autofac.Core;
 using Autofac.Extensions.DependencyInjection;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FluentUI.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,61 +26,72 @@ using System.Windows.Shapes;
 
 namespace APP
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
 
 
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddWpfBlazorWebView();
-            serviceCollection.AddBlazorWebViewDeveloperTools();
-         
-			
-            serviceCollection.AddFluentUIComponents();
-            serviceCollection.AddHttpClient();
-            var builder = new ContainerBuilder();
+			var serviceCollection = new ServiceCollection();
+			serviceCollection.AddWpfBlazorWebView();
+			serviceCollection.AddBlazorWebViewDeveloperTools();
 
-            builder.Populate(serviceCollection);
-            builder.AddAppService();
-            var autofac = new AutofacServiceProvider(builder.Build());
-            Resources.Add("services", autofac);
-            double x1 = SystemParameters.PrimaryScreenWidth * 0.6;//得到屏幕整体宽度
-            double y1 = SystemParameters.PrimaryScreenHeight * 0.6;//得到屏幕整体高度
-            this.Width = x1;//设置窗体宽度
-            this.Height = y1;//设置窗体高度
-            WindowsVariable.MainWindowHeight = this.Height;
-            WindowsVariable.MainWindowWidth = this.Width;
-            SizeChanged += OnWindowSizeChanged;
-            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
-            {
-                System.Windows.MessageBox.Show($"程序崩溃请重启{error}", "Error");
 
-            };
-            InitializeComponent();
-        }
-        protected void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (e.PreviousSize.Height != 0)
-            {
-                if (e.HeightChanged)
-                {
-                    WeakReferenceMessenger.Default.Send(new HeightChange(e.NewSize.Height));
-                    WindowsVariable.MainWindowHeight = e.NewSize.Height;
-                }
-            }
-            if (e.PreviousSize.Width != 0)
-            {
-                if (e.WidthChanged)
-                {
-                    WeakReferenceMessenger.Default.Send(new WidthChange(e.NewSize.Width));
-                    WindowsVariable.MainWindowWidth = e.NewSize.Width;
-                }
-            }
+			serviceCollection.AddFluentUIComponents();
+			serviceCollection.AddHttpClient();
+			var builder = new ContainerBuilder();
 
-        }
-    }
+			builder.Populate(serviceCollection);
+			builder.AddAppService();
+			var autofac = new AutofacServiceProvider(builder.Build());
+			Resources.Add("services", autofac);
+			double x1 = SystemParameters.PrimaryScreenWidth * 0.6;//得到屏幕整体宽度
+			double y1 = SystemParameters.PrimaryScreenHeight * 0.6;//得到屏幕整体高度
+			this.Width = x1;//设置窗体宽度
+			this.Height = y1;//设置窗体高度
+			WindowsVariable.MainWindowHeight = this.Height;
+			WindowsVariable.MainWindowWidth = this.Width;
+			SizeChanged += OnWindowSizeChanged;
+			AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+			{
+				System.Windows.MessageBox.Show($"程序崩溃请重启{error}", "Error");
+
+			};
+			InitializeComponent();
+		}
+		protected void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			if (e.PreviousSize.Height != 0)
+			{
+				if (e.HeightChanged)
+				{
+					WeakReferenceMessenger.Default.Send(new HeightChange(e.NewSize.Height));
+					WindowsVariable.MainWindowHeight = e.NewSize.Height;
+				}
+			}
+			if (e.PreviousSize.Width != 0)
+			{
+				if (e.WidthChanged)
+				{
+					WeakReferenceMessenger.Default.Send(new WidthChange(e.NewSize.Width));
+					WindowsVariable.MainWindowWidth = e.NewSize.Width;
+				}
+			}
+
+		}
+		protected override void OnContentRendered(EventArgs e)
+		{
+			base.OnContentRendered(e);
+			NonClientAreaContent = new NonClientAreaContent();
+
+		}
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			base.OnClosing(e);
+
+		}
+	}
 }
